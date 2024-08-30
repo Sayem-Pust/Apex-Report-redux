7c621bd6f72913f6c281c98277a5cfe1e68c936a
Sayem <mdsayem1629@gmail.com>
2024-08-31 01:50:01 +0600

    [Fix] user auth initially check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+'use client'
+import auth from "@/utils/auth";
 import { redirect } from "next/navigation";
 
 export default function Home() {
+  const user = auth.getUserInfo()
+  if (!user) { 
+    redirect("/login")
+  }
   redirect("/dashboard");
 }
