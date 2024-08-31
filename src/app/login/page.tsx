6cc736fe20ae6dc158674b14518188240e47badf
Sayem <mdsayem1629@gmail.com>
2024-08-31 11:19:16 +0600

    [enhancement] loading add in login button

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -14,7 +14,7 @@ const PoppinsFont = Poppins({
 });
 
 export default function Login() {
-  const { user, error } = useAppSelector((state) => state.users);
+  const { user, error, loading } = useAppSelector((state) => state.users);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState<{ email?: string; password?: string }>(
@@ -186,11 +186,40 @@ export default function Login() {
                     )}
                   </div>
                 </div>
-                <button
-                  className={`mt-0 lg:!mt-10 bg-blue-500 text-white py-2 px-6 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 disabled:bg-gray-400`}
-                >
-                  Sign In
-                </button>
+
+                {loading ? (
+                  <button
+                    className="inline-flex items-center justify-center mt-0 lg:!mt-10 bg-blue-500 text-white py-2 px-6 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 disabled:bg-gray-400"
+                  >
+                    <svg
+                      className="w-5 h-5 mr-3 -ml-1 text-black animate-spin"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        stroke-width="4"
+                      ></circle>
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      ></path>
+                    </svg>
+                    Loading...
+                  </button>
+                ) : (
+                  <button
+                    className="mt-0 lg:!mt-10 bg-blue-500 text-white py-2 px-6 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 disabled:bg-gray-400"
+                  >
+                    Sign In
+                  </button>
+                )}
               </form>
 
               {/* Display error message from API */}
